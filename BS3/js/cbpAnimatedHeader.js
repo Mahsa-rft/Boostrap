c47c2a97953211ce977ab52b476a012136a26f71
mahsa rafat <mahsa_rft@icloud.com>
2017-08-01 13:33:51 +0430

    the last one

--- BS3/js/cbpAnimatedHeader.js
+++ BS3/js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.cbp-af-header' ),
 		didScroll = false,
-		changeHeaderOn = 50;
+		changeHeaderOn = 300;
 
 	function init() {
 		scrollPage();
